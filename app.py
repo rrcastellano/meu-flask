b7d5744a5691ac82750ebea4831c811b3279b408
rrcastellano <51006960+rrcastellano@users.noreply.github.com>
2025-12-04 17:43:34 -0300

    atualização app.py sem debug

--- app.py
+++ app.py
@@ -1131,8 +1131,8 @@ def contact():
 
 # ----------------- RODA APLICACAO -----------------
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
 
 
